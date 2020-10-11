844b64b547d6a5dcb022035e6d5eb4c6e47c1dc7
Jean B <jean.barros.teixeira@gmail.com>
2020-10-11 11:46:02 -0300

    Add initial project structure

---
+++ db/migrate/20201011143515_create_contatos.rb
@@ -0,0 +1,10 @@
+class CreateContatos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :contatos do |t|
+      t.string :nome
+      t.string :telefone
+
+      t.timestamps
+    end
+  end
+end
